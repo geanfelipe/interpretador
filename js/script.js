7f069f530b4e1a1a1af83f7a406a0bf849529356
Manaia Junior <manaiajr.ifrn@hotmail.com>
2015-05-29 10:23:21 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.right.menu.open').on("click",function(e){
+        e.preventDefault();
+		$('.ui.horizontal.menu').toggle();
+	});
+    
+	$('.ui.dropdown').dropdown();
+});
