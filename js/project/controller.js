da9d6ac6f20cbd81dc071196074cda96bf65079d
unknown <franklin.dias@DESENV.pmsga.local>
2015-06-23 15:45:07 -0300

    Integração readJson com gerador html

--- js/project/controller.js
+++ js/project/controller.js
@@ -1,48 +1,54 @@
- angular.module("modelagem", []);
-            angular.module("modelagem").controller("modelagemCtrl", function ($scope) {
-              
-                $scope.app = "Modelagem de Páginas HTML";
+  angular.module("modelagem", []);
+  angular.module("modelagem").controller("modelagemCtrl", function($scope, $http) {
 
-                $scope.aplicativo = '';
-                $scope.segmentView  = '';
-                $scope.formView = '';
-                $scope.segAct = '';
+      $scope.app = "Modelagem de Páginas HTML";
 
-                $scope.secretarias = secretariasModel;
+      $scope.aplicativo = '';
+      $scope.segmentView = '';
+      $scope.formView = '';
+      $scope.segAct = '';
 
-                $scope.alternarAbaPrincipal = function (elementoComActive) {
-                    var abaAplicativo = angular.element(document.querySelector('#aba-de-aplicativo'));
-                    var abaPortalAdministrativo = angular.element(document.querySelector('#aba-do-portal-administrativo'));
-                    
-                    if(elementoComActive == 'aba-de-aplicativo'){
-                        abaAplicativo.addClass('active');
-                        abaPortalAdministrativo.removeClass('active');    
-                    }else {
-                        abaPortalAdministrativo.addClass('active');
-                        abaAplicativo.removeClass('active');    
-                    }
-                };
+      $http.get("../sgi/json/datamodel.json")
+          .success(function(data) {
+              $scope.secretarias = loadJson(data);
+          })
+          .catch(function() {
+              alert("Falhou ao carregar o arquivo JSON");
+          });
 
-                $scope.alternarAba = function (abaParaRemoverOActive, abaParaAdicionarOActive) {
-                    var elementoAbaParaRemoverOActive = angular.element(document.querySelector(abaParaRemoverOActive));
-                    var elementoAbaParaAdicionarOActive = angular.element(document.querySelector(abaParaAdicionarOActive));
-                    elementoAbaParaRemoverOActive.removeClass('block');
-                    elementoAbaParaRemoverOActive.addClass('none');
-                    elementoAbaParaAdicionarOActive.removeClass('none');
-                    elementoAbaParaAdicionarOActive.addClass('block');
-                };
+      $scope.alternarAbaPrincipal = function(elementoComActive) {
+          var abaAplicativo = angular.element(document.querySelector('#aba-de-aplicativo'));
+          var abaPortalAdministrativo = angular.element(document.querySelector('#aba-do-portal-administrativo'));
 
-                $scope.mudarAbaDeAplicativo = function (secretaria) {
-                    $scope.aplicativo =  secretaria.nome;
-                    $scope.segmentView = '';
-                };
+          if (elementoComActive == 'aba-de-aplicativo') {
+              abaAplicativo.addClass('active');
+              abaPortalAdministrativo.removeClass('active');
+          } else {
+              abaPortalAdministrativo.addClass('active');
+              abaAplicativo.removeClass('active');
+          }
+      };
 
-                $scope.mudarSegmentView = function (novoNomeSegmentView){
-                    $scope.segmentView = novoNomeSegmentView;
-                };
+      $scope.alternarAba = function(abaParaRemoverOActive, abaParaAdicionarOActive) {
+          var elementoAbaParaRemoverOActive = angular.element(document.querySelector(abaParaRemoverOActive));
+          var elementoAbaParaAdicionarOActive = angular.element(document.querySelector(abaParaAdicionarOActive));
+          elementoAbaParaRemoverOActive.removeClass('block');
+          elementoAbaParaRemoverOActive.addClass('none');
+          elementoAbaParaAdicionarOActive.removeClass('none');
+          elementoAbaParaAdicionarOActive.addClass('block');
+      };
 
-                $scope.mudarSegmentAct = function (novaSegmentAct){
-                    $scope.segAct = novaSegmentAct;
-                    console.log($scope.segAct);
-                };
-            });
+      $scope.mudarAbaDeAplicativo = function(secretaria) {
+          $scope.aplicativo = secretaria.nome;
+          $scope.segmentView = '';
+      };
+
+      $scope.mudarSegmentView = function(novoNomeSegmentView) {
+          $scope.segmentView = novoNomeSegmentView;
+      };
+
+      $scope.mudarSegmentAct = function(novaSegmentAct) {
+          $scope.segAct = novaSegmentAct;
+          console.log($scope.segAct);
+      };
+  });
--- js/readerJson.js
+++ js/readerJson.js
@@ -1,131 +1,119 @@
- angular.module("modelagem").controller("readerJsonCtrl", function($scope, $http) {
 
- 	//variável global
- 	$scope.secretariaModel = [];
+//variável global
+secretariasModel = [];
 
- 	//inicialização de variáveis
- 	var secretaria = {};
- 	var setores = [];
- 	var setorObj = {};
- 	var segments = [];
- 	var segmentObj = {};
- 	var inputs = [];
- 	var inputObj = {};
- 	var segmentsName = [];
+//inicialização de variáveis
+var secretaria = {};
+var setores = [];
+var setorObj = {};
+var segments = [];
+var segmentObj = {};
+var inputs = [];
+var inputObj = {};
+var segmentsName = [];
 
- 	//método para retornar elementos único de um array
- 	Array.prototype.getUnique = function() {
- 		var u = {},
- 			a = [];
- 		for (var i = 0, l = this.length; i < l; ++i) {
- 			if (u.hasOwnProperty(this[i])) {
- 				continue;
- 			}
- 			a.push(this[i]);
- 			u[this[i]] = 1;
- 		}
- 		return a;
- 	}
+//método para retornar elementos único de um array
+Array.prototype.getUnique = function() {
+	var u = {},
+		a = [];
+	for (var i = 0, l = this.length; i < l; ++i) {
+		if (u.hasOwnProperty(this[i])) {
+			continue;
+		}
+		a.push(this[i]);
+		u[this[i]] = 1;
+	}
+	return a;
+}
 
- 	//método para retonar inputs de um segmento em um grupo
- 	var getInputsBySegment = function(segment, list) {
+//método para retonar inputs de um segmento em um grupo
+var getInputsBySegment = function(segment, list) {
 
- 		//limpar variáveis
- 		inputs = [];
- 		inputObj = {};
+	//limpar variáveis
+	inputs = [];
+	inputObj = {};
 
- 		list.forEach(function(group) {
- 			angular.forEach(group.entities, function(entities) {
- 				angular.forEach(entities.attributes, function(attributes) {
+	list.forEach(function(group) {
+		angular.forEach(group.entities, function(entities) {
+			angular.forEach(entities.attributes, function(attributes) {
 
- 					//verifica se o input atual é do segmento desejado
- 					if (attributes.view.segment == segment) {
+				//verifica se o input atual é do segmento desejado
+				if (attributes.view.segment == segment) {
 
- 						inputObj["name"] = attributes.name;
- 						inputObj["title"] = attributes.view.title;
- 						inputObj["readonly"] = attributes.view.readonly;
- 						inputObj["show"] = attributes.view.show;
- 						inputObj["showAs"] = attributes.view.showAs;
- 						inputObj["defaultValue"] = attributes.view.defaultValue;
- 						inputObj["width"] = attributes.view.width;
- 						inputObj["mask"] = attributes.view.mask;
+					inputObj["name"] = attributes.name;
+					inputObj["title"] = attributes.view.title;
+					inputObj["readonly"] = attributes.view.readonly;
+					inputObj["show"] = attributes.view.show;
+					inputObj["showAs"] = attributes.view.showAs;
+					inputObj["defaultValue"] = attributes.view.defaultValue;
+					inputObj["width"] = attributes.view.width;
+					inputObj["mask"] = attributes.view.mask;
 
- 						inputs.push(angular.copy(inputObj));
+					inputs.push(angular.copy(inputObj));
 
- 						inputObj = {};
+					inputObj = {};
 
- 					}
- 				});
- 			});
- 		});
- 		return inputs;
- 	}
+				}
+			});
+		});
+	});
+	return inputs;
+}
 
- 	//função principal para leitura do json
- 	function loadJson() {
- 		//requisição ajax para baixar o arquivo json
- 		$http.get("../sgi/json/datamodel.json")
- 			.success(function(data) {
+//função principal para leitura do json
+function loadJson(data) {
+	//requisição ajax para baixar o arquivo json
+	//conversão do JSON para objeto javascript
+	dataModel = angular.fromJson(data);
 
- 				dataModel = angular.fromJson(data);
+	dataModel.groups.forEach(function(groups) {
 
- 				dataModel.groups.forEach(function(groups) {
+		//adiciona o nome das secretaria no json
+		secretaria["nome"] = groups.name;
 
- 					//adiciona o nome das secretaria no json
- 					secretaria["nome"] = groups.name;
+		//adiciona o nome dos setores no json
+		setorObj["nome"] = groups.setor;
 
- 					//adiciona o nome dos setores no json
- 					setorObj["nome"] = groups.setor;
+		angular.forEach(groups.fields, function(fields) {
+			angular.forEach(fields.entities, function(entities) {
+				angular.forEach(entities.attributes, function(attributes) {
 
-
- 					angular.forEach(groups.fields, function(fields) {
- 						angular.forEach(fields.entities, function(entities) {
- 							angular.forEach(entities.attributes, function(attributes) {
-
- 								//adiciona todos os nomes de segmentos possíveis encontrados no json
- 								segmentsName.push(attributes.view.segment);
+					//adiciona todos os nomes de segmentos possíveis encontrados no json
+					segmentsName.push(attributes.view.segment);
 
 
- 							});
- 						});
- 					});
+				});
+			});
+		});
 
- 					//varre um array retonado pelo getUnique, que por sua vez 
- 					//retorna um array de elementos não repetidos
- 					segmentsName.getUnique().forEach(function(segmentName) {
+		//varre um array retonado pelo getUnique, que por sua vez 
+		//retorna um array de elementos não repetidos
+		segmentsName.getUnique().forEach(function(segmentName) {
 
- 						segmentObj["nome"] = angular.copy(segmentName);
+			segmentObj["nome"] = angular.copy(segmentName);
 
- 						segmentObj["inputs"] = getInputsBySegment(segmentObj["nome"], groups.fields);
+			segmentObj["inputs"] = getInputsBySegment(segmentObj["nome"], groups.fields);
 
- 						segments.push(angular.copy(segmentObj));
+			segments.push(angular.copy(segmentObj));
 
- 					});
+		});
 
+		setorObj["segments"] = segments;
+		setores.push(angular.copy(setorObj));
+		secretaria["setor"] = setores;
 
- 					setorObj["segments"] = segments;
- 					setores.push(angular.copy(setorObj));
- 					secretaria["setores"] = setores;
+		secretariasModel.push(angular.copy(secretaria));
 
+		//limpeza de variáveis
+		secretaria = {};
+		setores = [];
+		setorObj = {};
+		segments = [];
+		segmentObj = {};
+		segmentsName = [];
 
- 					$scope.secretariaModel.push(angular.copy(secretaria));
-
- 					secretaria = {};
- 					setores = [];
- 					setorObj = {};
- 					segments = [];
- 					segmentObj = {};
- 					segmentsName = [];
-
- 				});
-
-
- 			})
- 			.catch(function() {
- 				alert("Falhou ao carregar o arquivo JSON");
- 			});
- 	}
-
- 	loadJson();
-
- });
+	});
+	
+	console.log(secretariasModel);
+	return secretariasModel;
+}
