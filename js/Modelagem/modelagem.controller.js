146ffc76653c27d1aa6c1f766ef0bbb853c2ce27
Gean Felipe <geanfelipemoura@outlook.com>
2015-08-17 12:25:49 -0300

    corrigido bug de troca de abas

--- js/Modelagem/modelagem.controller.js
+++ js/Modelagem/modelagem.controller.js
@@ -11,14 +11,19 @@ modelagemApp.controller('modelagemCtrl',[
       $scope.user ='';
       $scope.secretarias = '';
       var lista_de_secretarias= {};
-      
       var json =angular.fromJson(modelagemService.query());
-      
+    
+      $scope.trocar = function(secretaria){
+        $scope.aba=2;
+        $scope.tabAplicativo=true; 
+        $scope.secretariaSeleciodada=secretaria;
+      };
+          
       json.$promise.then(function(data){
         $scope.user = data.user;
         $scope.secretarias = Object.keys(data.groups);
         
-        //console.log($scope.secretarias);
+        console.log($scope.secretarias);
         
       });
     }
