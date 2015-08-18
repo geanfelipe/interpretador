d08eb45036c21f7558534edf8885e0de56ae69e2
Gean Felipe <geanfelipemoura@outlook.com>
2015-08-18 12:26:06 -0300

    feito feature de troca de abas

--- js/Modelagem/modelagem.controller.js
+++ js/Modelagem/modelagem.controller.js
@@ -1,30 +1,39 @@
 modelagemApp.controller('modelagemCtrl',[
     '$scope',
     'modelagemService',
-    '$log',
-    function($scope,modelagemService,$log){
-      $scope.$log = $log;
+    function($scope,modelagemService){
       $scope.aba = 1;
       $scope.tabAplicativo=false;
       $scope.secretaria ='';
       $scope.salvado='';
       $scope.user ='';
       $scope.secretarias = '';
+      $scope.secretariaSeleciodada=null;
+      $scope.formulariosDeSecretaria=null;
+      $scope.formulario='';
+      
       var lista_de_secretarias= {};
       var json =angular.fromJson(modelagemService.query());
     
-      $scope.trocar = function(secretaria){
-        $scope.aba=2;
-        $scope.tabAplicativo=true; 
+      $scope.trocarAba = function(secretaria){
         $scope.secretariaSeleciodada=secretaria;
+        $scope.aba = $scope.aba==1 ? $scope.aba=2:$scope.aba=1;
+        
+        if($scope.secretariaSeleciodada===null){
+          //FACA NADA
+        }else{
+          $scope.tabAplicativo = $scope.tabAplicativo===false ? $scope.tabAplicativo=true:$scope.tabAplicativo=false;
+        }
       };
-          
+      
       json.$promise.then(function(data){
         $scope.user = data.user;
         $scope.secretarias = Object.keys(data.groups);
-        
-        console.log($scope.secretarias);
-        
+        $scope.listarFormularioDeSecretaria = function(secretaria){
+          console.log(Object.keys(data.groups[secretaria]));
+          return Object.keys(data.groups[secretaria]);
+        };
+        $scope.lista = Object.keys(data.groups['sesad:almoxarifado']);
       });
     }
 ]);
