b2d5f61119ebd18c67a49c3b0100d6e15c86f027
Gean Felipe <geanfelipemoura@outlook.com>
2015-08-19 12:20:00 -0300

    modificado modelagem

--- js/Modelagem/modelagem.controller.js
+++ js/Modelagem/modelagem.controller.js
@@ -2,47 +2,48 @@ modelagemApp.controller('modelagemCtrl',[
     '$scope',
     'modelagemService',
     function($scope,modelagemService){
-      $scope.aba = 1;
-      $scope.tabAplicativo=false;
-      $scope.secretaria ='';
-      $scope.salvado='';
-      $scope.user ='';
-      $scope.secretarias = '';
-
-      $scope.secretariaSeleciodada='';
-
-      $scope.secretariaSeleciodada=null;
-      $scope.formulariosDeSecretaria=null;
-      $scope.formulario='';
+        $scope.aba = 1;
+        $scope.tabAplicativo=false;
+        $scope.secretaria ='';
+        $scope.salvado='';
+        $scope.user ='';
+        $scope.secretarias = '';
+        $scope.secretariaSeleciodada='';
+        $scope.secretariaSeleciodada=null;
+        $scope.formulariosDeSecretaria=null;
+        $scope.formulario='';
+        $scope.formularioSelecionado = false;
+        
+        var lista_de_secretarias= {};
+        var json =angular.fromJson(modelagemService.query());
       
-      var lista_de_secretarias= {};
-      var json =angular.fromJson(modelagemService.query());
-    
-      $scope.trocarAba = function(secretaria){
-        $scope.secretariaSeleciodada=secretaria;
-        $scope.aba = $scope.aba==1 ? $scope.aba=2:$scope.aba=1;
-        
-        if($scope.secretariaSeleciodada===null){
-          //FACA NADA
-        }else{
-          $scope.tabAplicativo = $scope.tabAplicativo===false ? $scope.tabAplicativo=true:$scope.tabAplicativo=false;
-        }
-      };
-      
-      json.$promise.then(function(data){
-        
-        $scope.user = data.user;
-        
-        $scope.secretarias = Object.keys(data.groups);
-        
-        $scope.listarFormularioDeSecretaria = function(secretaria){
-          if(secretaria!==null){
-            console.log(Object.keys(data.groups[secretaria]));
-            return Object.keys(data.groups[secretaria]);
-          }
+        $scope.trocarAba = function(secretaria){
+            $scope.secretariaSeleciodada=secretaria;
+            $scope.aba = $scope.aba==1 ? $scope.aba=2:$scope.aba=1;
+            
+            if($scope.secretariaSeleciodada===null){
+              //FACA NADA
+            }else{
+              $scope.tabAplicativo = $scope.tabAplicativo===false ? $scope.tabAplicativo=true:$scope.tabAplicativo=false;
+            }
         };
         
-        $scope.lista = Object.keys(data.groups['sesad:almoxarifado']);
-      });
-    }
+        /*retornado o json faca as seguintes operacoes*/
+        /*Vide: o promise é a ultima coisa que é carregada no controller*/
+        json.$promise.then(function(data){
+          
+            $scope.user = data.user;
+            
+            $scope.secretarias = Object.keys(data.groups);
+            
+            $scope.listarFormularioDeSecretaria = function(secretaria){
+                if(secretaria!==null){
+                  console.log(Object.keys(data.groups[secretaria]));
+                  return Object.keys(data.groups[secretaria]);
+                }
+            };
+            
+            $scope.lista = Object.keys(data.groups['sesad:almoxarifado']);
+        });
+      }
 ]);
