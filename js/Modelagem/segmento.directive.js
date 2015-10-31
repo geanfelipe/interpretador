3542ac7da3923b6d450bacde10a233cb9d056478
Gean Felipe <geanfelipemoura@outlook.com>
2015-10-31 04:21:08 -0300

    diretiva de construcao de segmentos OK

--- js/Modelagem/segmento.directive.js
+++ js/Modelagem/segmento.directive.js
@@ -3,7 +3,7 @@ angular.module("ModelagemDirectives",[])
 .directive("segmentos",function() {
 	return {
 		restrict: "E",
-		transclude: true,
+		transclude: "element",
 		replace: true,
 		scope : {
 			data:"=",
@@ -11,16 +11,44 @@ angular.module("ModelagemDirectives",[])
 		compile: function($element,$attrs,transcludeFn) {
 			return function($scope,el,$attrs) {
 				transcludeFn($scope, function (cElement) {
+
+					
+
+					$scope.$watch(function(scope){
+						if(typeof scope.segmentos==="object") {
+							for(var i in scope.segmentos) {
+
+								var conteudoDaTAG = Object.keys(scope.segmentos[i]);
+								var  contextmenu = scope.segmentos[i][conteudoDaTAG];
+
+								var tagA = document.createElement("a");
+								tagA.setAttribute("contextmenu",contextmenu);
+								tagA.textContent = conteudoDaTAG;
+								tagA.className="item";
+
+								$element
+								.after(tagA);
+							}
+						}
+					})
               		// <a ng-repeat="(key,value) in campoDeFormulario" contextmenu="{{value.contextmenu}}"  class="item">{{key}}</a>
-              		// var tagA = document.createElement("a");
+              		// 
               		// tagA.
           		});
 			}
 		},
 		controller: function($scope,$element,$attrs) {
+			$scope.segmentos = [];
+
 			$scope.$watch(function(scope){
-				/*console.log($scope.data);
-				console.log($scope.tipo);*/
+				if(typeof scope.data ==="object") {
+					angular.forEach(scope.data,function(value,key) {
+						var obj = {};
+						obj[key]=value.contextmenu
+						//{nome_do_segmento : nome_do_contextmenu}
+						scope.segmentos.push(obj);
+					});
+				}
 			});
 		},
 	}
