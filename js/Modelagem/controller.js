c7ca114e6ae227ed2955d698894b58deef58fe53
Danilo Medeiros <danilomedeiros03@gmail.com>
2015-11-04 12:32:05 -0300

    modificao de teste para paginacao da tabela

--- js/Modelagem/controller.js
+++ js/Modelagem/controller.js
@@ -69,30 +69,67 @@ modelagemApp.controller("modelagemCtrl",[
 
         };
 
-        $scope.sortType     = 'id'; // set the default sort type
-        $scope.sortReverse  = false;  // set the default sort order
 
-        $scope.pessoal = [{id: "01", nome : "Joaquim", sexo: "Masculino"}, 
+
+//este bloco simplesmente serve para realizar as funções de ordenar e de listar os valores da tabela.
+
+        $scope.sortType     = 'id'; // define o atributo que servira de parametro para a listagem
+        $scope.sortReverse  = false;  // define se a listagem será na ordem normal (true) ou inversa(false)
+
+        //sera alterado posteriormente para o dado recebido do get
+        $scope.pessoal = [{id: "01", nome : "Joaquim Teixeira", sexo: "Masculino"}, 
                             {id: "03", nome : "Aparecida", sexo: "Masculino"}, 
-                            {id: "02", nome : "Raiane", sexo: "Feminino"}];
+                            {id: "02", nome : "Raiane Karla Miranda Silva", sexo: "Feminino"},
+                            {id: "04", nome : "Marcos", sexo: "Masculino"},
+                            {id: "05", nome : "Clayton", sexo: "Masculino"},
+                            {id: "06", nome : "Fulano", sexo: "Masculino"},
+                            {id: "07", nome : "Beltrano", sexo: "Masculino"},
+                            {id: "08", nome : "Sicrano", sexo: "Masculino"},
+                            {id: "09", nome : "Maria", sexo: "Feminino"},
+                            {id: "10", nome : "Tereza", sexo: "Feminino"},
+                            {id: "11", nome : "Almeida", sexo: "Masculino"},
+                            {id: "12", nome : "Rosa", sexo: "Feminino"}];
 
-        $scope.atributos = Object.keys($scope.pessoal[0]);
+        $scope.atributos = Object.keys($scope.pessoal[0]); //cria um array com os atributos dos objetos
 
-        for (atributo in $scope.atributos){
+        for (atributo in $scope.atributos){     //coloca a primeira letra maiscula
             $scope.atributos[atributo]= $scope.atributos[atributo].capitalizeFirstLetter();
         }
-        
+      
+
+        //esta funcao define o sortType e e diparada a partir do ng-click do cabecalho da coluna do respectivo atributo
+
         $scope.setSortType = function(atributo){
 
+            $scope.selecao = atributo; //variavel que recebe o atributo com letra maiscula
+                                        //e e usada para mudar a classe do icone do cabecalho
+
             $scope.sortType = atributo.toLowerCase();
-                
-            console.log($scope.sortType);
 
-            $scope.sortReverse = !$scope.sortReverse;
-
-            
+            $scope.sortReverse = !$scope.sortReverse;   //inverte o valor do sortReverse
                     
         }
+
+        $scope.paginas = [];
+        $scope.pagina = [];
+        $scope.itemsPorPagina = 7;
+        var contador = 0;
+
+        for (item in $scope.pessoal){
+            if (contador<$scope.itemsPorPagina && item<$scope.pessoal.length){
+                contador++
+                $scope.pagina.push($scope.pessoal[item]);
+                
+            } else{
+                contador=0;
+                $scope.paginas.push($scope.pagina);
+                $scope.pagina = [];
+            }
+        }
+        
+        console.log($scope.paginas);
+
+//FIM DO BLOCO DE FUNCOES RELACIONADAS A TABELA
 
 
         /*retornado o json faca as seguintes operacoes*/
