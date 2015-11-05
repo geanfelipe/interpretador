c5e34cf2fc2f712cfef7eda4c728312ae0968529
gean felipe <geanfelipemoura@gmail.com>
2015-11-05 10:11:53 -0300

    refeito estrutura de diretorios

--- js/Modelagem/Modelagem.App.js
+++
@@ -1,5 +0,0 @@
-var modelagemApp = angular.module('Modelagem',[]);
-
-modelagemApp.run(function($rootScope,modelagemService){
-   $rootScope.json = angular.fromJson(modelagemService.query());
-});
--- js/Modelagem/filter.uppercasefirstchar.js
+++
@@ -1,6 +0,0 @@
-angular.module('ModelagemFilters',[])
-.filter('firstUpper',function(){
-	return function(input){
-		return input.capitalizeFirstLetter();
-	}
-})
