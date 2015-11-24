fd47820360d02aea749a159cffb805279b732ae9
gean felipe <geanfelipemoura@gmail.com>
2015-11-24 09:25:24 -0300

    adicionado ngroute para furuta implementacao

--- scripts/main.app.js
+++ scripts/main.app.js
@@ -2,8 +2,18 @@ var mainApp= angular.module('mainApp',[
   'Modelagem',
   'Services',
   'ModelagemFilters',
+  'ngRoute',
   ]);
 
-mainApp.config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.debugInfoEnabled(false);
-}]);
+mainApp.config(['$compileProvider','$routeProvider','$locationProvider', 
+	function ($compileProvider,$routeProvider,$locationProvider) {
+    	$compileProvider.debugInfoEnabled(false);
+		
+		$routeProvider
+			.when("/", {
+				controller: 'ModelagemController',
+				template:'/home.html'
+			})
+			.otherwise({redirectTo: '/'});
+	}
+]);
